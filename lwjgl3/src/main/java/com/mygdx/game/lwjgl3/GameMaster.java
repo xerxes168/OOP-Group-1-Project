cf6d06e83a4a0447bc49a27889006303518463f8
ngtrolling <ngtrolling1208@gmail.com>
2025-01-17 16:06:29 +0800

    Added test bucket on screen

---
+++ lwjgl3/src/main/java/com/mygdx/game/lwjgl3/GameMaster.java
@@ -0,0 +1,43 @@
+package com.mygdx.game.lwjgl3;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+public class GameMaster extends ApplicationAdapter {
+	
+	private Texture bucketImage;
+	private SpriteBatch batch;
+	
+	@Override
+	public void create ()
+	{
+		bucketImage = new Texture(Gdx.files.internal("testbuck.png"));
+		batch = new SpriteBatch();	
+	}
+
+	@Override
+	public void render()
+	{
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		
+		batch.begin();
+
+			batch.draw(bucketImage, 150, 150, 50, 50);
+			
+		batch.end();
+
+			
+	}
+	
+	@Override
+	public void dispose() {
+		batch.dispose();
+		bucketImage.dispose();
+		
+	}
+}
+
--- lwjgl3/src/main/java/com/mygdx/game/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/mygdx/game/lwjgl3/Lwjgl3Launcher.java
@@ -12,7 +12,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new MyGdxGame(), getDefaultConfiguration());
+        return new Lwjgl3Application(new GameMaster(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
