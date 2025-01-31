1a18232551bfd220ba12409b03073951bb3e3a8a
ngtrolling <ngtrolling1208@gmail.com>
2025-02-01 00:17:04 +0800

    tried to add barrier

--- lwjgl3/src/main/java/com/mygdx/game/lwjgl3/Character.java
+++ lwjgl3/src/main/java/com/mygdx/game/lwjgl3/Character.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Character extends Entity implements iMovable, iCollidable {
 	private float currentxPos;
 	private float currentyPos;
+	private boolean inScreen;
 	
 	Character() {
 		super();
@@ -32,15 +33,31 @@ public class Character extends Entity implements iMovable, iCollidable {
 	
 	@Override
 	public void movement() {
-		currentyPos = super.getY();
-		if (Gdx.input.isKeyPressed(Keys.UP)) currentyPos += 200 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Keys.DOWN)) currentyPos -= 200 * Gdx.graphics.getDeltaTime();
-		super.setY(currentyPos);
+		if(inScreen == true) {
+			currentyPos = super.getY();
+			if (Gdx.input.isKeyPressed(Keys.UP)) currentyPos += 200 * Gdx.graphics.getDeltaTime();
+			if (Gdx.input.isKeyPressed(Keys.DOWN)) currentyPos -= 200 * Gdx.graphics.getDeltaTime();
+			super.setY(currentyPos);
+			
+			currentxPos = super.getX();
+			if (Gdx.input.isKeyPressed(Keys.LEFT)) currentxPos -= 200 * Gdx.graphics.getDeltaTime();
+			if (Gdx.input.isKeyPressed(Keys.RIGHT)) currentxPos += 200 * Gdx.graphics.getDeltaTime();
+			super.setX(currentxPos);
+		}
 		
-		currentxPos = super.getX();
-		if (Gdx.input.isKeyPressed(Keys.LEFT)) currentxPos -= 200 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Keys.RIGHT)) currentxPos += 200 * Gdx.graphics.getDeltaTime();
-		super.setX(currentxPos);	
+		if (super.getX() >= 560) {
+			inScreen = false;
+		} else {
+			inScreen = true;
+		}
+		
+		if (super.getY() < 0) {
+			inScreen = false;
+		} else {
+			inScreen = true;
+		}
+	
+		
 	}
 	
 	@Override
