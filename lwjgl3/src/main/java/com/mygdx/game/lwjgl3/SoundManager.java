fea26ecaa98660f0585c3085096369ebd1272503
xerxes168 <2402253@sit.singaporetech.edu.sg>
2025-02-14 12:41:20 +0800

    fixed minor error for soundmanager

--- lwjgl3/src/main/java/com/mygdx/game/lwjgl3/SoundManager.java
+++ lwjgl3/src/main/java/com/mygdx/game/lwjgl3/SoundManager.java
@@ -16,9 +16,10 @@ public class SoundManager {
 	private HashMap<String, Clip> soundMap;  // Stores sound clips for easy access
     private float masterVolume = 1.0f; // master volume for all sound range from 0.0 to 1.0
     
-}
+
 
 	public SoundManager() {
-		SoundMap= new HashMap<>();
+		soundMap= new HashMap<>();
 	}
 
+}
